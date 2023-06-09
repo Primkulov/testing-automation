623b9f8c69c1b3cc9a5c6d62d1ae13d7a0f64554
tynchprim <primkulov060690@gmail.com>
2023-06-08 21:12:01 -0500

    initial commit

---
+++ src/test/java/page/GoogleHomePage.java
@@ -0,0 +1,11 @@
+package page;
+
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class GoogleHomePage {
+
+    public GoogleHomePage(){
+       PageFactory.initElements(Driver.getDriver(),this);
+    }
+}
